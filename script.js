2c076605403be8bc322e9b3a3d2e3a6c7e5b8884
raizalas <ashari.raizal17@gmail.com>
2024-04-09 14:16:20 +0700

    added basic input to display

---
+++ script.js
@@ -0,0 +1,11 @@
+function addToResult(value) {
+  document.querySelector("#result").value += value;
+}
+
+function clearResult() {
+  document.querySelector("#result").value = "";
+}
+
+function operate (operator, numberOne, numberTwo) {
+    return 
+}
